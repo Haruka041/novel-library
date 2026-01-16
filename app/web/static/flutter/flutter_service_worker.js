a42143d6b321bee90e201c3c5f2a0c18d4e84667
Haruka041 <harukasakura@foxmail.com>
2026-01-16 16:40:49 +0800

    fix: 禁用Service Worker缓存解决路由不更新问题

--- app/web/static/flutter/flutter_service_worker.js
+++ app/web/static/flutter/flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "2fc47c0a0c3c7af8542b601634fe9674",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "flutter.js": "59a12ab9d00ae8f8096fffc417b6e84f",
-"index.html": "07b815a2491a074155005e7125d1dbd1",
-"/": "07b815a2491a074155005e7125d1dbd1",
+"index.html": "2ee12ed05199f5eea47b62ee7fae2608",
+"/": "2ee12ed05199f5eea47b62ee7fae2608",
 "main.dart.js": "5e7412cd9d69cd2efba47de955bc4610",
 "manifest.json": "a88e44f95b3ebcc2def87ddfd0ed9447",
 "version.json": "b22274a8834ac2796bb36c4f63e1ddb4"};
